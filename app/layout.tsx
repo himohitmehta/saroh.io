02079a8224d768f2e07907103d936e634fc0662f
Mohit Mehta <mehtam2701@gmail.com>
2023-09-23 18:49:36 +0530

    [Added]: theme toggle component to setup theme in the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Saroh.io - Coming Soon',
-  description: 'Build your online storefronts with Saroh.io',
-}
+	title: "Saroh.io - Coming Soon",
+	description: "Build your online storefronts with Saroh.io",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { ThemeToggle } from "@/components/common/ThemeToggle";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Saroh.io: Coming Soon
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			Saroh.io: Coming Soon
+			<ThemeToggle />
+		</main>
+	);
 }
