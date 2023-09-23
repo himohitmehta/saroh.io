ba0db4e524c63c2f7e99132b6895ce2379b9dc47
Mohit Mehta <mehtam2701@gmail.com>
2023-09-23 19:42:01 +0530

    added routing config and domain config to the app, following similar structure as dub.co

--- app/saroh.io/page.tsx
+++ app/saroh.io/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Saroh.io: Coming Soon
-			<ThemeToggle />
+			
+			
 		</main>
 	);
 }
