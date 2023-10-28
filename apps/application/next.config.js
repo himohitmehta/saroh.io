3c1a930cb5e5fec7e76a22e63be7acd553efea7c
Mohit Mehta <mehtam2701@gmail.com>
2023-10-28 23:56:01 +0530

    website template added, project made similar to vercel's platfroms repo

--- apps/application/next.config.js
+++ apps/application/next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
 		serverActions: true,
 	},
 	reactStrictMode: false,
+	images: {
+		domains: [
+			"public.blob.vercel-storage.com",
+			"res.cloudinary.com",
+			"abs.twimg.com",
+			"pbs.twimg.com",
+			"avatars.githubusercontent.com",
+			"www.google.com",
+			"flag.vercel.app",
+			"illustrations.popsy.co",
+			"lh3.googleusercontent.com",
+		],
+	},
 };
 
 module.exports = nextConfig;
