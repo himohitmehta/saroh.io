6e34ef29c1a5cca0dd1939dd9acb3ea8ecc47f2d
Mohit Mehta <mehtam2701@gmail.com>
2023-10-29 22:10:41 +0530

    updated the `web` app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,31 @@
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["ui"],
+/** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
+const nextConfig = {
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()];
+		}
+
+		return config;
+	},
+	reactStrictMode: false,
+	experimental: {
+		serverActions: true,
+	},
+	images: {
+		domains: [
+			"public.blob.vercel-storage.com",
+			"res.cloudinary.com",
+			"abs.twimg.com",
+			"pbs.twimg.com",
+			"avatars.githubusercontent.com",
+			"www.google.com",
+			"flag.vercel.app",
+			"illustrations.popsy.co",
+			"lh3.googleusercontent.com",
+		],
+	},
 };
+
+module.exports = nextConfig;
