5386eb5abe71bb63836c98814505cc22c0f822f2
Mohit Mehta <mehtam2701@gmail.com>
2023-10-29 20:32:10 +0530

    [Test]: found the error was schema.prisma got deleted accidentally

--- apps/sites/next.config.js
+++ apps/sites/next.config.js
@@ -13,6 +13,19 @@ const nextConfig = {
 	experimental: {
 		serverActions: true,
 	},
+	images: {
+		domains: [
+			"public.blob.vercel-storage.com",
+			"res.cloudinary.com",
+			"abs.twimg.com",
+			"pbs.twimg.com",
+			"avatars.githubusercontent.com",
+			"www.google.com",
+			"flag.vercel.app",
+			"illustrations.popsy.co",
+			"lh3.googleusercontent.com",
+		],
+	},
 };
 
 module.exports = nextConfig;
