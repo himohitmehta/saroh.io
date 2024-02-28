906acfb14f7263d8088434793b376ba4796ab8c7
Mohit Mehta <mehtam2701@gmail.com>
2024-02-28 22:21:39 +0530

    added few components for the landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,18 @@
-import Hero from "@/components/home/hero";
+import { BentoGridThirdDemo } from "@/components/home/bento-grid-demo";
+import { CardHoverEffectDemo } from "@/components/home/card-hover-demo";
+import { SparklesPreview } from "@/components/home/sparkles-preview";
+import { SpotlightPreview } from "@/components/home/spotlight";
 
 export default function Page(): JSX.Element {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Hero />
+		<main className="flex min-h-screen flex-col items-center justify-between ">
+			{/* <NewHero /> */}
+			{/* <Hero /> */}
+			{/* <GoogleGeminiEffectDemo /> */}
+			<SpotlightPreview />
+			<BentoGridThirdDemo />
+			<CardHoverEffectDemo />
+			<SparklesPreview />
 		</main>
 	);
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,12 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const {
+	default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+const svgToDataUri = require("mini-svg-data-uri");
+
+const config = {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
+	prefix: "",
 	theme: {
 		container: {
 			center: true,
@@ -58,19 +65,88 @@ module.exports = {
 			},
 			keyframes: {
 				"accordion-down": {
-					from: { height: 0 },
+					from: { height: "0" },
 					to: { height: "var(--radix-accordion-content-height)" },
 				},
 				"accordion-up": {
 					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
+					to: { height: "0" },
+				},
+				scroll: {
+					to: {
+						transform: "translate(calc(-50% - 0.5rem))",
+					},
+				},
+				spotlight: {
+					"0%": {
+						opacity: "0",
+						transform: "translate(-72%, -62%) scale(0.5)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translate(-50%,-40%) scale(1)",
+					},
+				},
+				meteor: {
+					"0%": {
+						transform: "rotate(215deg) translateX(0)",
+						opacity: "1",
+					},
+					"70%": { opacity: "1" },
+					"100%": {
+						transform: "rotate(215deg) translateX(-500px)",
+						opacity: "0",
+					},
 				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+				spotlight: "spotlight 2s ease .75s 1 forwards",
+				"meteor-effect": "meteor 5s linear infinite",
 			},
 		},
 	},
-	// plugins: [require("tailwindcss-animate")],
-};
+	plugins: [
+		require("tailwindcss-animate"),
+		addVariablesForColors,
+		function ({ matchUtilities, theme }: any) {
+			matchUtilities(
+				{
+					"bg-grid": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+						)}")`,
+					}),
+					"bg-grid-small": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+						)}")`,
+					}),
+					"bg-dot": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+						)}")`,
+					}),
+				},
+				{
+					values: flattenColorPalette(theme("backgroundColor")),
+					type: "color",
+				},
+			);
+		},
+	],
+} satisfies Config;
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
+
+export default config;
