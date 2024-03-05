d77ca19f9b3b30752aefe3b4c4c8b774e13bf0ad
Mohit Mehta <mehtam2701@gmail.com>
2024-03-05 16:19:02 +0530

    test deployment on vercel with prisma

--- apps/web/components/home/bento-grid-demo.tsx
+++ apps/web/components/home/bento-grid-demo.tsx
@@ -292,23 +292,23 @@ const SkeletonFive = () => {
 				variants={variants}
 				className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black"
 			>
-				<Image
+				{/* <Image
 					src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
 					alt="avatar"
 					height="100"
 					width="100"
 					className="rounded-full h-10 w-10"
-				/>
-				<p className="text-xs text-neutral-500">
+				/> */}
+				{/* <p className="text-xs text-neutral-500">
 					There are a lot of cool framerworks out there like React,
 					Angular, Vue, Svelte that can make your life ....
-				</p>
+				</p> */}
 			</motion.div>
 			<motion.div
 				variants={variantsSecond}
 				className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
 			>
-				<p className="text-xs text-neutral-500">Use PHP.</p>
+				{/* <p className="text-xs text-neutral-500">Use PHP.</p> */}
 				<div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
 			</motion.div>
 		</motion.div>
