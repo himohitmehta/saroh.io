ae93eaf13ac20c5da1e198d982bc1a5d8bf1d1ce
Mohit Mehta <mehtam2701@gmail.com>
2024-06-02 18:54:53 +0530

    updated ui package and dashboard to test if ui is installed correctly

--- apps/dashboard/postcss.config.mjs
+++ apps/dashboard/postcss.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
 
-export default config;
+// export default config;
+export * from "@saroh/ui/postcss.config";
