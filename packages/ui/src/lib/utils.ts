f71386cae37bc74a57baca4f151c66207899f15f
Mohit Mehta <mehtam2701@gmail.com>
2024-06-02 20:14:26 +0530

    updated the ui config, now styles are showing up

--- packages/ui/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type  -- not added because it's a utility function that doesn't need a return type
- */
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
