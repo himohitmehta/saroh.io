9c071e73f698fd531755dc94760036f0fc975dee
Mohit Mehta <mehtam2701@gmail.com>
2024-06-10 22:56:07 +0530

    auth package updated, Not in use

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,17 +1,12 @@
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import NextAuth, { NextAuthResult } from "next-auth";
-// import GitHub from "next-auth/providers/github";
-// import Google from "next-auth/providers/google";
-// import prisma from "./prisma";
-// const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { NextAuthResult } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import prisma from "./prisma";
+const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
 
-// export const nextAuthInstance: NextAuthResult = NextAuth({
-//     providers: [GitHub, Google],
-//     adapter: PrismaAdapter(prisma),
-//     session: { strategy: "jwt" },
-// });
-
-// export const auth = nextAuthInstance.auth;
-// export const handlers = nextAuthInstance.handlers;
-// export const signIn = nextAuthInstance.signIn;
-// export const signOut = nextAuthInstance.signOut;
+export const nextAuthInstance: NextAuthResult = NextAuth({
+    providers: [GitHub, Google],
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt" },
+});
--- packages/auth/tsup.config.ts
+++ packages/auth/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-    entry: ["src/index.ts"],
+    entry: ["src/index.tsx"],
     format: ["esm", "cjs"],
-    // esbuildOptions(options) {
-    //     options.banner = {
-    //         js: '"use client"',
-    //     };
-    // },
+    esbuildOptions(options) {
+        options.banner = {
+            js: '"use client"',
+        };
+    },
     dts: true,
     target: "es2019",
     clean: true,
