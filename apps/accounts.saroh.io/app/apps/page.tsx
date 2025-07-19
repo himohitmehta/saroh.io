a1ccb365b48de939a7a337b66b4aa54ef94a706b
Mohit Mehta <mehtam2701@gmail.com>
2025-07-19 16:33:16 +0530

    updated prisma schema from MySQL to Postgre

--- apps/accounts.saroh.io/app/apps/page.tsx
+++ apps/accounts.saroh.io/app/apps/page.tsx
@@ -1,22 +1,77 @@
+"use client";
+import { Button } from "@saroh/ui/button";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 
 export default function AppsListPage() {
     return (
-        <div className="flex flex-col gap-2 p-8 justify-center items-center">
+        <div className="flex flex-col items-center justify-center gap-2 p-8">
+            <Button onClick={() => signOut()}>Sign Out</Button>
             <div className="flex flex-col gap-2 text-2xl font-bold">
                 Select the app to open
             </div>
             <div className="flex flex-col gap-2 p-8 ">
-                <Link href="http://localhost:3001" className="hover:bg-gray-100 rounded-md p-2">Admin</Link>
-                <Link href="http://localhost:3002" className="hover:bg-gray-100 rounded-md p-2">AI</Link>
-                <Link href="http://localhost:3003" className="hover:bg-gray-100 rounded-md p-2">Application</Link>
-                <Link href="http://localhost:3004" className="hover:bg-gray-100 rounded-md p-2">Chatbot</Link>
-                <Link href="http://localhost:3005" className="hover:bg-gray-100 rounded-md p-2">Dashboard</Link>
-                <Link href="http://localhost:3006" className="hover:bg-gray-100 rounded-md p-2">Docs</Link>
-                <Link href="http://localhost:3007" className="hover:bg-gray-100 rounded-md p-2">Email</Link>
-                <Link href="http://localhost:3008" className="hover:bg-gray-100 rounded-md p-2">Sites</Link>
-                <Link href="http://localhost:3009" className="hover:bg-gray-100 rounded-md p-2">Templates</Link>
-                <Link href="http://localhost:3010" className="hover:bg-gray-100 rounded-md p-2">UI</Link>
+                <Link
+                    href="http://localhost:3001"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Admin
+                </Link>
+                <Link
+                    href="http://localhost:3002"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    AI
+                </Link>
+                <Link
+                    href="http://localhost:3003"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Application
+                </Link>
+                <Link
+                    href="http://localhost:3004"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Chatbot
+                </Link>
+                <Link
+                    href="http://localhost:3005"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Dashboard
+                </Link>
+                <Link
+                    href="http://localhost:3006"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Docs
+                </Link>
+                <Link
+                    href="http://localhost:3007"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Email
+                </Link>
+                {/* <Link href="http://localhost:3008" className="hover:bg-gray-100 rounded-md p-2">Website</Link> */}
+                <Link
+                    href="http://localhost:3009"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Sites
+                </Link>
+                <Link
+                    href="http://localhost:3010"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    Templates
+                </Link>
+                <Link
+                    href="http://localhost:3011"
+                    className="rounded-md p-2 hover:bg-gray-100"
+                >
+                    UI
+                </Link>
             </div>
         </div>
     );
--- apps/accounts.saroh.io/app/layout.tsx
+++ apps/accounts.saroh.io/app/layout.tsx
@@ -19,7 +19,12 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Providers>{children}</Providers>
+                <Providers>
+                    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>{" "}
+                    <div className="relative z-10 h-screen w-full">
+                        {children}
+                    </div>
+                </Providers>
             </body>
         </html>
     );
