c0fd2465ff6ac21419695de2ca44dfcf123ef29b
Mohit Mehta <mehtam2701@gmail.com>
2025-07-20 15:02:34 +0530

    updated applinks

--- apps/accounts.saroh.io/app/apps/page.tsx
+++ apps/accounts.saroh.io/app/apps/page.tsx
@@ -3,75 +3,84 @@ import { Button } from "@saroh/ui/button";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 
+const apps = [
+    {
+        name: "Admin",
+        devUrl: "http://localhost:3001",
+        prodUrl: "https://admin.saroh.io",
+    },
+    {
+        name: "AI",
+        devUrl: "http://localhost:3002",
+        prodUrl: "https://ai.saroh.io",
+    },
+    {
+        name: "Application",
+        devUrl: "http://localhost:3003",
+        prodUrl: "https://application.saroh.io",
+    },
+    {
+        name: "Chatbot",
+        devUrl: "http://localhost:3004",
+        prodUrl: "https://chatbot.saroh.io",
+    },
+    {
+        name: "Dashboard",
+        devUrl: "http://localhost:3005",
+        prodUrl: "https://dashboard.saroh.io",
+    },
+    {
+        name: "Docs",
+        devUrl: "http://localhost:3006",
+        prodUrl: "https://docs.saroh.io",
+    },
+    {
+        name: "Email",
+        devUrl: "http://localhost:3007",
+        prodUrl: "https://email.saroh.io",
+    },
+    {
+        name: "Sites",
+        devUrl: "http://localhost:3009",
+        prodUrl: "https://sites.saroh.io",
+    },
+    {
+        name: "Templates",
+        devUrl: "http://localhost:3010",
+        prodUrl: "https://templates.saroh.io",
+    },
+    {
+        name: "UI",
+        devUrl: "http://localhost:3011",
+        prodUrl: "https://ui.saroh.io",
+    },
+    {
+        name: "Website",
+        devUrl: "http://localhost:3012",
+        prodUrl: "https://website.saroh.io",
+    },
+];
+
 export default function AppsListPage() {
+    const isProduction = process.env.NODE_ENV === "production";
     return (
-        <div className="flex flex-col items-center justify-center gap-2 p-8">
-            <Button onClick={() => signOut()}>Sign Out</Button>
-            <div className="flex flex-col gap-2 text-2xl font-bold">
-                Select the app to open
-            </div>
-            <div className="flex flex-col gap-2 p-8 ">
-                <Link
-                    href="http://localhost:3001"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Admin
-                </Link>
-                <Link
-                    href="http://localhost:3002"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    AI
-                </Link>
-                <Link
-                    href="http://localhost:3003"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Application
-                </Link>
-                <Link
-                    href="http://localhost:3004"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Chatbot
-                </Link>
-                <Link
-                    href="http://localhost:3005"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Dashboard
-                </Link>
-                <Link
-                    href="http://localhost:3006"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Docs
-                </Link>
-                <Link
-                    href="http://localhost:3007"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Email
-                </Link>
-                {/* <Link href="http://localhost:3008" className="hover:bg-gray-100 rounded-md p-2">Website</Link> */}
-                <Link
-                    href="http://localhost:3009"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Sites
-                </Link>
-                <Link
-                    href="http://localhost:3010"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    Templates
-                </Link>
-                <Link
-                    href="http://localhost:3011"
-                    className="rounded-md p-2 hover:bg-gray-100"
-                >
-                    UI
-                </Link>
+        <div className="flex flex-col items-center justify-center gap-2 p-8 ">
+            <div className="flex flex-col items-center justify-center gap-2 rounded-md border border-gray-200 bg-white p-8">
+                <Button onClick={() => signOut()}>Sign Out</Button>
+                <div className="flex flex-col gap-2 text-2xl font-bold">
+                    Select the app to open
+                </div>
+                <div className="flex flex-col gap-2 p-8 ">
+                    {apps.map((app) => (
+                        <Link
+                            key={app.name}
+                            href={isProduction ? app.prodUrl : app.devUrl}
+                            className="rounded-md p-2 hover:bg-gray-100"
+                        >
+                            {app.name}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
